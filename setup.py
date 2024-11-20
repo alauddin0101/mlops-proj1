21c0660b9892d37400f6bf6cff0e8fc398799da2
alauddin0101 <alauddin.ml0101@gmail.com>
2024-11-21 03:04:01 +0600

    project structure rady and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="alauddin0101",
+    author_email="alauddin.ml0101@gmail.com",
+    packages=find_packages()
+)
